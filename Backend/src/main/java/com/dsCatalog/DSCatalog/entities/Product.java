670f86e0237abd1fb24d5d990bb5f34e81e98638
oherbert <o_herbert@hotmail.com>
2020-11-20 17:10:23 -0300

    Project Created

---
+++ Backend/src/main/java/com/dsCatalog/DSCatalog/entities/Product.java
@@ -0,0 +1,107 @@
+package com.dsCatalog.DSCatalog.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String descripton;
+    private Double price;
+    private String imgUrl;
+
+    private List<Category> categories = new ArrayList<>();
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String descripton, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.descripton = descripton;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+    
+    
+
+}
---
+++ Backend/src/test/java/com/dsCatalog/DSCatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsCatalog.DSCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
